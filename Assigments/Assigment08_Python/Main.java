ad240df017ddb9b2f44602ae27a4db9dcb4098f3
jorgera10 <jorgeramosgc@gmail.com>
2019-10-17 23:52:09 -0500

    version 1.0

---
+++ Assigments/Assigment08_Python/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] arg) {
+        Cuadrilatero c1 = new Cuadrilatero();
+
+        float lado1 = 10, lado2 = 20;
+
+        System.out.println("El perimetro del cuadrilatero es: " + c1.getPerimetro());
+        System.out.println("El area del cuadrilatero es: " + c1.getArea());
+
+    }
+}
