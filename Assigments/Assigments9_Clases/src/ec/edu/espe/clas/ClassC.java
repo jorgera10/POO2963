9e3b04228aca6ab375526222fc091bbe4b5592e5
jorgera10 <jorgeramosgc@gmail.com>
2019-11-12 18:41:30 -0500

    version 1

---
+++ Assigments/Assigments9_Clases/src/ec/edu/espe/clas/ClassC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.clas;
+
+/**
+ *
+ * @author Jorge Ramos
+ */
+public class ClassC {
+
+	private ClassB[] b;
+
+	/**
+	 * 
+	 * @param e
+	 */
+	public void m(ClassE e) {
+		// TODO - implement ClassC.m
+		throw new UnsupportedOperationException();
+	}
+
+}
